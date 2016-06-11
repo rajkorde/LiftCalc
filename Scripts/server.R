b0268426031f51d2e7ab4a76253d87dfe2c22a22
rajkorde <rkorde39@hotmail.com>
2016-06-11 16:18:01 -0700

    minor edits

--- Scripts/server.R
+++ Scripts/server.R
@@ -1,4 +1,9 @@
 library(shiny)
+library(CausalImpact)
+
+# By default, the file size limit is 5MB. It can be changed by
+# setting this option. Here we'll raise limit to 25MB.
+options(shiny.maxRequestSize = 25*1024^2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -17,5 +22,61 @@ shinyServer(function(input, output) {
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
+    
+    output$contents <- renderTable({
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, it will be a data frame with 'name',
+        # 'size', 'type', and 'datapath' columns. The 'datapath'
+        # column will contain the local filenames where the data can
+        # be found.
+        
+        inFile <- input$file1
+        
+        if (is.null(inFile))
+            return(NULL)
+        
+        read.csv(inFile$datapath, header=FALSE)
+    })
+    
+    output$impact <- renderPlot({
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, it will be a data frame with 'name',
+        # 'size', 'type', and 'datapath' columns. The 'datapath'
+        # column will contain the local filenames where the data can
+        # be found.
+        
+        inFile <- input$file1
+        
+        if (is.null(inFile))
+            return(NULL)
+        
+        d = read.csv(inFile$datapath, header=FALSE, col.names=
+                         c("Date", "Control", "Treat"))
+        d$Date = as.Date(d$Date, format="%Y-%m-%d")
+        
+        pre.period = c(as.Date("2016-05-10", format="%Y-%m-%d"),
+                       as.Date("2016-05-23", format="%Y-%m-%d"))
+        post.period = c(as.Date("2016-05-24", format="%Y-%m-%d"),
+                        as.Date("2016-06-04", format="%Y-%m-%d"))
+        
+        
+        z = zoo(cbind(y=d$Treat, x=d$Control), d$Date)
+        set.seed(420)
+        impact = CausalImpact(z, pre.period, post.period)
+        plot(impact)        
+    })
+    
+    output$prevalue  <- renderText({
+        paste("input$predates is", 
+              paste(as.character(input$predates), collapse = " to ")
+        )
+    })
+    
+    output$postvalue  <- renderText({
+        paste("input$postdates is", 
+              paste(as.character(input$postdates), collapse = " to ")
+        )
+    })
+    
     
 })
--- Scripts/ui.R
+++ Scripts/ui.R
@@ -13,12 +13,31 @@ shinyUI(fluidPage(
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 30),
+            dateRangeInput("predates", 
+                           label = "Pre-Experiment Period",
+                           start = Sys.Date()-28,
+                           end = Sys.Date()-15),
+            dateRangeInput("postdates", 
+                           label = "Experiment Period",
+                           start = Sys.Date()-14,
+                           end = Sys.Date()),
+            fileInput('file1', 'Choose file to upload',
+                      accept = c(
+                          'text/csv',
+                          'text/comma-separated-values',
+                          '.csv'
+                      )
+            )
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("distPlot"),
+            tableOutput('contents'),
+            verbatimTextOutput("prevalue"),
+            verbatimTextOutput("postvalue"),
+            plotOutput("impact")
         )
     )
 ))
